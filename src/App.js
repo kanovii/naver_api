f4316bd778984f53c7f2c3958b51df77cdd64b89
kanovii <kkh0510ks@daum.net>
2022-03-20 13:56:51 +0900

    delete useless import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Home from "./routes/Home"
-import Movies from "./components/Movies"
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
